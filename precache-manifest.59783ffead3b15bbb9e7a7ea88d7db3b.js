03d0d30e51083322af38e25ff421860225bc70d3
Evan Charlton <evancharlton@microsoft.com>
2020-10-24 23:34:38 +0200

    Updates

---
+++ precache-manifest.59783ffead3b15bbb9e7a7ea88d7db3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3596a45cb14e68675fbd74df2e87ff",
+    "url": "/zipper/index.html"
+  },
+  {
+    "revision": "0d87f9f6d1742f3b2994",
+    "url": "/zipper/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "0100fe399054c1f7864a",
+    "url": "/zipper/static/css/main.fa91a461.chunk.css"
+  },
+  {
+    "revision": "0d87f9f6d1742f3b2994",
+    "url": "/zipper/static/js/2.2273705e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/zipper/static/js/2.2273705e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0100fe399054c1f7864a",
+    "url": "/zipper/static/js/main.c78a9421.chunk.js"
+  },
+  {
+    "revision": "1cbb9e905ab3f37a6e56",
+    "url": "/zipper/static/js/runtime-main.824785ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zipper/precache-manifest.c6d6898714e778fe954c78963d6cfbb1.js"
+  "/zipper/precache-manifest.59783ffead3b15bbb9e7a7ea88d7db3b.js"
 );
 
 self.addEventListener('message', (event) => {
